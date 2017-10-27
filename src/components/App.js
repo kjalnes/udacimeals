bc9ed3d72937afe4c5cae5815ca0414d1b14b52b
kjalnes <kris.alnes@gmail.com>
2017-10-27 13:55:14 -0400

    feat: Redux store implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,80 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addRecipe, removeFromCalendar } from '../actions';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
+import { capitalize } from '../utils/helper'
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
 
 class App extends Component {
-
   render() {
-    console.log(this.props)
+    const { calendar, remove } = this.props
+    const mealOrder = ['breakfast', 'lunch', 'dinner']
+
+
     return (
-      <div>
-        Hello
+      <div className='container'>
+
+        <ul className='meal-types'>
+          {mealOrder.map((mealType) => (
+            <li key={mealType} className='subheader'>
+              {capitalize(mealType)}
+            </li>
+          ))}
+        </ul>
+
+        <div className='calendar'>
+          <div className='days'>
+            {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+          </div>
+          <div className='icon-grid'>
+            {calendar.map(({ day, meals }) => (
+              <ul key={day}>
+                {mealOrder.map((meal) => (
+                  <li key={meal} className='meal'>
+                    {meals[meal]
+                      ? <div className='food-item'>
+                          <img src={meals[meal].image} alt={meals[meal].label}/>
+                          <button onClick={() => remove({meal, day})}>Clear</button>
+                        </div>
+                      : <button className='icon-btn'>
+                          <CalendarIcon size={30}/>
+                        </button>}
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
+
       </div>
     )
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        selectRecipe: (data) => dispatch(addRecipe(data)),
-        remove: (data) => dispatch(removeFromCalendar(data))
-    }
+function mapStateToProps ({ food, calendar }) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? food[calendar[day][meal]]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
 }
 
-
-const mapStateToProps = (calendar) => {
-    const dayOrder = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-    return {
-        calendar: dayOrder.map( day => ({
-            day,
-            meals: Object.keys(calendar[day]).reduce( (meals, meal) => {
-                meals[meal] = calendar[day][meal]
-                    ? calendar[day][meal]
-                    : null
-                return meals
-            },{})
-        }))
-    }
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,21 @@ import {
     REMOVE_FROM_CALENDAR
 } from '../actions';
 
+import { combineReducers } from 'redux';
+
+const food = (state={}, action) => {
+  switch(action.type) {
+    case ADD_RECIPE:
+      const { recipe } = action;
+
+      return {
+        ...state,
+        [recipe.label]: recipe
+      }
+    default:
+      return state;
+  }
+}
 
 const initialCalendarState = {
   sunday: {
@@ -67,4 +82,7 @@ function calendar(state = initialCalendarState, action) {
     }
 }
 
-export default calendar;
+export default combineReducers({
+  food,
+  calendar
+});
