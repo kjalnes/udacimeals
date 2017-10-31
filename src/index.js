d5880c3b488dad12370b0b9b9bcce982cf62cb3d
kjalnes <kris.alnes@gmail.com>
2017-10-31 13:38:54 -0400

    Final project commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
 import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 
+
+
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+}
+
+const reduxLogger = createLogger()
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    composeEnhancers(
+        applyMiddleware(reduxLogger)
+        // applyMiddleware(logger)
+    )
+)
 
 // console.log(store.getState());
 
